8283bd10022c06800dae852d5868e6f34b0fe9f5
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-08-31 15:33:23 -0600

    feat: add game logic
    Check if pokemon was seen before and update a 'correct' state accordingly
    before passing it down to Score component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,6 +10,7 @@ const REGION = 'kanto';
 function Game() {
     const [pokemonData, setPokemonData] = useState([]);
     const [seenPokemon, setSeenPokemon] = useState([]);
+    const [correct, setCorrect] = useState(null);
 
     useEffect(() => {
         (async () => {
@@ -22,20 +23,32 @@ function Game() {
         return <p>Loading...</p>
     }
 
-    function handleNew() {
-        setSeenPokemon([...seenPokemon, name]);
+    const addUnseen = function addIfPokemonNotSeenBefore() {
+        if (!seenPokemon.includes(name)) {
+            setSeenPokemon([...seenPokemon, name]);
+        }
     }
-
+    
+    function handleSeen() {
+        addUnseen();
+        setCorrect(seenPokemon.includes(name));
+    }
+    
+    function handleNew() {
+        addUnseen();
+        setCorrect(!seenPokemon.includes(name));
+    }
+    
     const currPokemon = pokemonData[Math.floor(Math.random() * pokemonData.length)];
     const [name, src] = Object.entries(currPokemon)[0];
     return (
         <div className='game'>
-            <Score />
+            <Score correct={correct} />
 
             <Card name={name} src={src} />
 
             <div className="btns">
-                <button>Seen</button>
+                <button onClick={handleSeen}>Seen</button>
                 <button onClick={handleNew}>New</button>
             </div>
         </div>
