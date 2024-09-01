494f3322830d37ac1540df0eec5df450028aac7b
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-09-01 10:30:46 -0600

    refactor: remove redundant polyfills

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import cjs from "@rollup/plugin-commonjs";
 
 
@@ -19,18 +18,7 @@ export default defineConfig({
       buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
       events: "rollup-plugin-node-polyfills/polyfills/events",
       util: "rollup-plugin-node-polyfills/polyfills/util",
-      sys: "util",
       stream: "rollup-plugin-node-polyfills/polyfills/stream",
-      _stream_duplex:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-      _stream_passthrough:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-      _stream_readable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-      _stream_writable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-      _stream_transform:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
     },
   },
   build: {
@@ -38,9 +26,6 @@ export default defineConfig({
       plugins: [
         // Enable rollup polyfills plugin
         // used during production bundling
-        nodePolyfills({
-          include: ["node_modules/**/*.js", "../../node_modules/**/*.js"],
-        }),
         cjs(),
       ],
     },
